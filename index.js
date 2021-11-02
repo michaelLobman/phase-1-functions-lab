32858570744e7bac6076f97f31488d3fb6cff5b5
Michael Lobman <michael.lobman@gmail.com>
2021-11-02 11:00:51 -0400

    commit tree af23ce2234e26b9022c93d30086c7360f616fd3c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+	return Math.abs(42 - blocks)
+}
+
+function distanceFromHqInFeet(blocks){
+	return Math.abs((42 - blocks)*264)
+}
+
+function distanceTravelledInFeet(origin, destination){
+	return Math.abs((destination - origin) * 264)
+}
+
+function calculatesFarePrice(start, destination){
+	const distance = Math.abs(destination - start) * 264;
+	if (distance < 400) {
+		return 0
+	} else if (distance > 400 && distance < 2000) {
+		return (distance - 400) * .02
+	} else if (distance > 2000 && distance < 2500) {
+		return 25
+	} else if (distance > 2500) {
+		return 'cannot travel that far'
+	}
+} 
